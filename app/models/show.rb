e330cee6f235d837bd8a2321d927e96b6893a519
Benjamin Jones <bljones36@gmail.com>
2018-11-10 14:53:24 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,9 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum('rating')
   end
+
+  def self.highest_rating
+    show = self.order(rating: :desc).limit(1)
+    show.name
+  end
 end
