2c85e54385ca6e6591258573cee7c31fa6bb1655
Benjamin Jones <bljones36@gmail.com>
2018-11-10 14:49:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    self.select(:rating).order(rating: :desc).limit(1)
+    self.select_value(:rating).order(rating: :desc).limit(1)
   end
 end
