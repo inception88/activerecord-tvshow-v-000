18ed6382ded3357db71c1658e64eb56be4fc98f8
Benjamin Jones <bljones36@gmail.com>
2018-11-10 14:03:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,5 +3,5 @@ class CreateShows < ActiveRecord::Migration
   create_table :shows do |t|
     t.string :name
     t.string :network
-    t.datetime :day
-    t.string :rating
+    t.string :day
+    t.integer :rating
